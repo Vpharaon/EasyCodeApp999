3ed67127e9340a39a859227e7618491e560a6ca0
Viacheslav <viacheslav.farionik@gmail.com>
2023-10-17 21:22:12 +0300

    add click on textView

--- app/src/main/java/com/farionik/easycodeapp999/MainActivity.kt
+++ app/src/main/java/com/farionik/easycodeapp999/MainActivity.kt
@@ -2,10 +2,17 @@ package com.farionik.easycodeapp999
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.setOnClickListener {
+            
+        }
     }
 }
