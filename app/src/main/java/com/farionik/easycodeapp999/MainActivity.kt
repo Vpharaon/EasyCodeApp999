26ecee5d7893e63d9657b5a08146a007734a40a0
Viacheslav <viacheslav.farionik@gmail.com>
2023-10-17 21:28:16 +0300

    find button in activity

--- app/src/main/java/com/farionik/easycodeapp999/MainActivity.kt
+++ app/src/main/java/com/farionik/easycodeapp999/MainActivity.kt
@@ -2,6 +2,7 @@ package com.farionik.easycodeapp999
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,5 +15,7 @@ class MainActivity : AppCompatActivity() {
         textView.setOnClickListener {
             Toast.makeText(it.context, "Click on view", Toast.LENGTH_SHORT).show()
         }
+
+        val button = findViewById<Button>(R.id.button)
     }
 }
